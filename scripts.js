1acf506860897d2588526bf76d3d34d157fd3ff5
cosmobalance <148830972+cosmobalance@users.noreply.github.com>
2023-10-23 23:20:09 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,170 @@
+const CONVERSION_RATES = {
+    "USDT_TO_EURO": 0.944,
+    "USDT_TO_USDT": 1,
+    "EURO_TO_EURO": 1,
+    "EURO_TO_USDT": 1.06,
+    "CREDITOS_TO_EURO": 1.4 / 50,
+    "CREDITOS_TO_USDT": 1.5 / 50,
+    "USDT_TO_CREDITOS": 50 / 1.5,
+    "EURO_TO_CREDITOS": 50 / 1.4
+};
+
+let saldo_jugadorA = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+let saldo_jugadorB = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    let conversionKey = `${fromCurrency}_TO_${toCurrency}`;
+    let rate = CONVERSION_RATES[conversionKey] || 1;
+    return Math.round(amount * rate);
+}
+
+function calculateBets() {
+    let apuestaA = parseFloat(document.getElementById("apuestaA").value);
+    let monedaA = document.getElementById("monedaA").value;
+    let monedaB = document.getElementById("monedaB").value;
+    
+    let apuestaB = convertCurrency(apuestaA, monedaA, monedaB);
+    
+    document.getElementById("apuestaBOutput").textContent = `La apuesta convertida del jugador B es: ${apuestaB} ${monedaB}`;
+}
+
+// ... (código previo)
+
+function getCommission(amount, currency) {
+    switch (currency) {
+        case "USDT":
+        case "EURO":
+            return Math.floor(amount * 0.1);
+            
+        case "CREDITOS":
+            return calcularComisionCreditos(amount);
+        default:
+            return 0;
+    }
+}
+
+// scripts.js
+
+// ... (otros códigos)
+
+/**
+ * Calcula el valor basado en bloques de créditos.
+ * Los créditos se dividen en bloques de 500 a partir de los 1000 créditos.
+ * @param {number} cantidad_creditos - El total de créditos a evaluar.
+ * @return {number|null} - El valor calculado o null si la cantidad es menor a 1000.
+ */
+function calcularValor(cantidad_creditos) {
+    const CREDITO_MINIMO = 1000;
+    const TAMANO_BLOQUE = 500;
+    const VALOR_BASE = 100;
+    const VALOR_POR_BLOQUE = 50;
+    
+    if (cantidad_creditos < CREDITO_MINIMO) {
+        return null; // o cualquier otro valor por defecto
+    }
+    
+    let bloque = Math.floor((cantidad_creditos - CREDITO_MINIMO) / TAMANO_BLOQUE);
+    return VALOR_BASE + VALOR_POR_BLOQUE * bloque;
+}
+
+function calcularComisionCreditos(cantidad_creditos) {
+    if (cantidad_creditos >= 1000) {
+        return calcularValor(cantidad_creditos);
+    }
+
+    // Aquí incluyes toda la lógica anterior que proporcionaste
+    if (10 <= cantidad_creditos && cantidad_creditos <= 14) return 3;
+    if (15 <= cantidad_creditos && cantidad_creditos <= 19) return 4;
+    if (20 <= cantidad_creditos && cantidad_creditos <= 25) return 5;
+    if (26 <= cantidad_creditos && cantidad_creditos <= 35) return 6;
+    if (36 <= cantidad_creditos && cantidad_creditos <= 39) return 7;
+    if (40 <= cantidad_creditos && cantidad_creditos <= 45) return 8;
+    if (46 <= cantidad_creditos && cantidad_creditos <= 49) return 9;
+    if (50 <= cantidad_creditos && cantidad_creditos <= 99) return 10;
+    if (100 <= cantidad_creditos && cantidad_creditos <= 149) return 20;
+    if (150 <= cantidad_creditos && cantidad_creditos <= 499) return 30;    
+    if (500 <= cantidad_creditos && cantidad_creditos <= 999) return 50;
+    // ... (resto del código)
+    
+    else {
+        return 0; // En caso de que no entre en ningún rango, regresar 0
+    }
+}
+
+// ... (otros códigos)
+
+
+
+function declareWinner() {
+    let apuestaA = parseFloat(document.getElementById("apuestaA").value);
+    let monedaA = document.getElementById("monedaA").value;
+    let monedaB = document.getElementById("monedaB").value;
+    let apuestaB = convertCurrency(apuestaA, monedaA, monedaB);
+    let ganador = document.getElementById("ganador").value;
+
+    let saldoGanado, comision;
+    if (ganador === "A") {
+        comision = getCommission(apuestaA, monedaA);
+        saldoGanado = apuestaA + apuestaA - comision;
+        saldo_jugadorA[monedaA] += saldoGanado;
+        saldo_jugadorB[monedaB] -= apuestaB;
+    } else {
+        comision = getCommission(apuestaB, monedaB);
+        saldoGanado = apuestaB + apuestaB - comision;
+        saldo_jugadorB[monedaB] += saldoGanado;
+        saldo_jugadorA[monedaA] -= apuestaA;
+    }
+
+    displayResults();
+}
+
+function displayResults() {
+    let resultadosDiv = document.getElementById("resultados");
+    resultadosDiv.innerHTML = `
+        <h2>SALDOS</h2>
+        <h3>Saldo Jugador A: USDT: ${saldo_jugadorA['USDT']}, EURO: ${saldo_jugadorA['EURO']}, CREDITOS: ${saldo_jugadorA['CREDITOS']}</h3>
+        <h3>Saldo Jugador B: USDT: ${saldo_jugadorB['USDT']}, EURO: ${saldo_jugadorB['EURO']}, CREDITOS: ${saldo_jugadorB['CREDITOS']}</h3>
+    `;
+}
+
+function resetSaldoA() {
+    saldo_jugadorA = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+    document.getElementById("apuestaA").value = '';
+    displayResults();
+}
+
+function resetSaldoB() {
+    saldo_jugadorB = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+    document.getElementById("apuestaA").value = '';
+    displayResults();
+}
+
+function resetAll() {
+    saldo_jugadorA = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+    saldo_jugadorB = {"USDT": 0, "EURO": 0, "CREDITOS": 0};
+    document.getElementById("apuestaA").value = '';
+    document.getElementById("apuestaBOutput").textContent = '';
+    displayResults();
+}
+
+$(document).ready(function() {
+    function formatState (state) {
+        if (!state.id) { return state.text; }
+        var imgUrl = $(state.element).data('thumbnail');
+        var $state = $(
+            '<span><img src="' + imgUrl + '" class="img-flag" /> ' + state.text + '</span>'
+        );
+        return $state;
+    };
+
+    function formatStateSelection (state) {
+        if (!state.id) { return state.text; }
+        var imgUrl = $(state.element).data('thumbnail');
+        return $('<span><img src="' + imgUrl + '" class="img-flag" /> ' + state.text + '</span>');
+    }
+
+    $(".divisas").select2({
+        templateResult: formatState,
+        templateSelection: formatStateSelection
+    });
+});
